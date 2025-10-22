8bd92a1639c781c742f5614a9fe01637cf329d15
Joe Mason <joewebdev87@gmail.com>
2025-10-22 18:09:10 -0500

    add CRUD view

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Community
+from .serializers import UserSerializer, CommunitySerializer
 
 class UserList(generics.ListCreateAPIView):
     """List all Users or create a new User."""
@@ -14,3 +14,11 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = [IsAuthenticated]  # Add later
+
+class CommunityList(generics.ListCreateAPIView):
+    queryset = Community.objects.all()
+    serializer_class = CommunitySerializer
+
+class CommunityDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Community.objects.all()
+    serializer_class = CommunitySerializer
