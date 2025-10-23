98d39bf66d6cd6dda06ddddff723cd52b0b58eb2
Joe Mason <joewebdev87@gmail.com>
2025-10-23 18:56:53 -0500

    remaining endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import UserList, UserDetail, CommunityList, CommunityDetail
+from .views import (
+    UserList,
+    UserDetail,
+    CommunityList,
+    CommunityDetail,
+    PostList,
+    PostDetail,
+    CommentList,
+    CommentDetail,
+)
 
 app_name = 'api'
 
@@ -8,4 +17,8 @@ urlpatterns = [
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
     path('communities/', CommunityList.as_view(), name='community-list'),
     path('communities/<int:pk>/', CommunityDetail.as_view(), name='community-detail'),
+    path('posts/', PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('comments/', CommentList.as_view(), name='comment-list'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
 ]
