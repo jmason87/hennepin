1275eb9afe2dd585d111cfafafa9cf08cd0667fc
Joe Mason <joewebdev87@gmail.com>
2025-10-23 18:57:00 -0500

    remaining views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import User, Community
-from .serializers import UserSerializer, CommunitySerializer
+from .models import User, Community, Post, Comment
+from .serializers import (
+    UserSerializer,
+    CommunitySerializer,
+    PostSerializer,
+    CommentSerializer,
+)
 
 class UserList(generics.ListCreateAPIView):
     """List all Users or create a new User."""
@@ -22,3 +27,23 @@ class CommunityList(generics.ListCreateAPIView):
 class CommunityDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Community.objects.all()
     serializer_class = CommunitySerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
