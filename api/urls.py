8e205bf715e89fbff5f42f29038291d608010f49
Joe Mason <joewebdev87@gmail.com>
2025-11-05 11:56:02 -0600

    update serializer and urlconf

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import (
     UserList,
     UserDetail,
     CommunityList,
     CommunityDetail,
-    PostList,
-    PostDetail,
+    PostViewSet,
     CommentList,
     CommentDetail,
     RegisterView,
@@ -13,14 +13,18 @@ from .views import (
 
 app_name = 'api'
 
+# Router for ViewSets
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+
 urlpatterns = [
     path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
     path('communities/', CommunityList.as_view(), name='community-list'),
     path('communities/<int:pk>/', CommunityDetail.as_view(), name='community-detail'),
-    path('posts/', PostList.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
     path('comments/', CommentList.as_view(), name='comment-list'),
     path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
     path("auth/register/", RegisterView.as_view(), name="auth_register"),
+
+    path('', include(router.urls))
 ]
